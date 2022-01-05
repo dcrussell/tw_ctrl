154f0c2f26c7eed260d0409e84cb9c49a84f3fe3
Dalton Russell <drussell@fastmail.com>
2022-01-05 08:14:34 -0500

    Now requires that the config is in the executable's directory

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
+use std::env;
 use std::error::Error;
+use std::path::PathBuf;
 use std::process;
 use tw_ctrl::config::Config;
 use tw_ctrl::log;
 
 //TODO: Add logger for output
 fn main() {
-    let config =
-        Config::new(&"/home/dalton/Projects/tw_ctrl/config".to_string()).unwrap_or_else(|err| {
-            log::fatal(&format!(
-                "Failed opening config file -- {}",
-                err.to_string()
-            ));
-            process::exit(1);
-        });
+    let mut dir = env::current_exe().expect("How did we get here?");
+    dir.pop();
+    dir.push("config");
+    let config = Config::new(&dir.to_str().unwrap().to_string()).unwrap_or_else(|err| {
+        log::fatal(&format!(
+            "Failed opening config file -- {}",
+            err.to_string()
+        ));
+        process::exit(1);
+    });
 
     // Run the controller
     if let Err(e) = tw_ctrl::run(config) {
